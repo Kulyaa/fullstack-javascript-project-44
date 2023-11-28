e4e20d42cce3709e2aa38f7279c9b005d4b2d15c
Kulya <Kulya15@yandex.ru>
2023-11-28 23:38:26 +0300

    add gcd

---
+++ bin/brain-gcd.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+import gcd from '../games/gcd.js';
+gcd();
---
+++ games/gcd.js
@@ -0,0 +1,23 @@
+import randNumber from '../src/randNumber.js';
+import startGame from '../src/index.js';
+
+const comment = 'Find the greatest common divisor of given numbers.';
+
+const gameLogic = () => {
+    let randNum1 = randNumber(1, 50);
+    let randNum2 = randNumber(1, 50);
+    const questions = `${randNum1} ${randNum2}`;
+    while (randNum1 != 0 & randNum2 != 0){
+        if(randNum1 > randNum2) {
+            randNum1 = randNum1 % randNum2;
+        }
+        else{
+            randNum2 = randNum2 % randNum1;
+        }
+    }
+    let correctAnswer = randNum1 + randNum2;
+    
+    
+    return [questions, String(correctAnswer)];
+}
+export default () => startGame(comment, gameLogic);
