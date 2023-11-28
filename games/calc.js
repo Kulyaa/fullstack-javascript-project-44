893f6baf850128bb43f114a122dceb4a0a1c1e76
Kulya <Kulya15@yandex.ru>
2023-11-28 22:42:23 +0300

    add brain-calc

---
+++ games/calc.js
@@ -0,0 +1,28 @@
+import randNumber from '../src/randNumber.js';
+import startGame from '../src/index.js';
+
+const comment = 'What is the result of the expression?';
+
+const gameLogic = () => {
+    const operators = ['+', '-', '*'];
+    const randNum1 = randNumber(1, 50);
+    const randNum2 = randNumber(1, 50);
+    let correctAnswer = 0;
+    const randOperator = operators[randNumber(0, operators.length - 1)];
+    switch (randOperator) {
+        case "+" :
+            correctAnswer = randNum1 + randNum2;
+            break;
+        case "-" :
+            correctAnswer = randNum1 - randNum2;
+            break;
+        case "*" :
+            correctAnswer = randNum1 * randNum2;
+            break;
+        default :
+            break;
+    }
+    const questions = `${randNum1}${randOperator}${randNum2}`;
+    return [questions, String(correctAnswer)];
+}
+export default () => startGame(comment, gameLogic);
