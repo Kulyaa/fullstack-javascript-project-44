f95726a2642f33c49f09c4288d7ffbeed30fd75b
Kulya <Kulya15@yandex.ru>
2023-12-04 15:55:54 +0300

    fix prime

--- games/prime.js
+++ games/prime.js
@@ -4,6 +4,9 @@ import startGame from '../src/index.js';
 const comment = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 const gameLogic = () => {
   const questions = randNumber(1, 100);
+  if (questions < 2) {
+    return [String(questions), 'no'];
+  }
   for (let i = 2; i < questions; i += 1) {
     if (questions % i === 0) return [String(questions), 'no'];
   }
