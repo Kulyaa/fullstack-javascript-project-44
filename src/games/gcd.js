5afe31a4b045fba8daa5a047d0cc8e0d50fdf026
Kulya <Kulya15@yandex.ru>
2024-01-17 23:31:46 +0300

    fix gcd

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -13,8 +13,8 @@ const getGcd = (num1, num2) => {
 const generateRound = () => {
   const randNum1 = randNumber(1, 50);
   const randNum2 = randNumber(1, 50);
-  const questions = getGcd(randNum1, randNum2);
-  const correctAnswer = randNum1 + randNum2;
+  const questions = `${randNum1} ${randNum2}`;
+  const correctAnswer = getGcd();
 
   return [questions, String(correctAnswer)];
 };
